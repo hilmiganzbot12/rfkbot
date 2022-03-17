75e6963425ff9d7636c1919fc862b669ae03b4ed
hilmiganzbot12 <96254454+hilmiganzbot12@users.noreply.github.com>
2022-03-17 20:40:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ gc1 = 'https://chat.whatsapp.com/Jxe836o7w9L9EhaYPkyZdV'
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whats', 'https://chat.whatsap'] // ganti jadi group lu
-global.owner = ['6283822759168'] // Put your number here //owner eval
-global.kontak = ['6283822759168','0'] //Ketika ada yang ngetik #owner
+global.owner = ['6281391745329' ,'18642108663 '] // Put your number here //owner eval
+global.kontak = ['6281391745329','18642108663'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['6283822759168'] // Premium user has unlimited limit
+global.prems = ['18642108663'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -47,20 +47,20 @@ xteamkey = 'Dawnfrostkey'
 namaig = 'GAK ADA :)'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Nih kak`
-namakontak1 = 'Andrii Street Moonshine'
-namakontak2 = 'Andrii Street Moonshine'
+namakontak1 = 'Owner Farel Botz'
+namakontak2 = 'Black BotZ Official'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By Andrii Street Moonshine' // ganti aja
-global.author = 'Andrii Street Moonshine' // ganti aja
+global.packname = 'Farel botz' // ganti aja
+global.author = 'Black BotZ Official' // ganti aja
 
 //yyy
-bc = 'RFK' //ANDRIIxBOTZ Broadcast
-footer = '\n©ANDRIIxBOTZ'
-namabot = 'ANDRIIxBOTZ'
-namalu = 'Andrii Street Moonshine'
+bc = 'Farel Botz' //ANDRIIxBOTZ Broadcast
+footer = '\n©Black BotZ Official'
+namabot = 'Farel Botz'
+namalu = 'Farel Ganz'
 
 
 // 
